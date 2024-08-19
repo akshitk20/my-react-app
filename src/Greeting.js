7104cc744a56b958ff2f8630d7969cb32821492b
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-19 21:09:19 +0530

    code refactored

--- src/Greeting.js
+++ src/Greeting.js
@@ -5,11 +5,13 @@ export const Greeting = () => {
     let greetingHeader = isMorning
     ?  <h3>Good Morning!</h3>  
     :  <h3>Good Evening!</h3> 
-    return 
-    <>
-       {greetingHeader} 
-      <h3>You have 5 new messages !</h3>
-    </>
+    return (
+     <>
+        {greetingHeader} 
+       <h3>You have 5 new messages !</h3>
+     </>
+    );
+ 
 }
 
 //export default Greeting;
