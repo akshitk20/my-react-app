90bc7ade4b9b44b2d47e339c6d625a832ccebbf3
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-19 20:42:05 +0530

    Update App.js with variables

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const isMorning = new Date().getHours();
+  let greeting = isMorning < 12 ?
+  <h3>Good Morning!</h3> :
+  <h3>Good Evening!</h3>;
+  const learning = "LEARNING";
+  const url = "https://reactjs.org";
   return (
     <div className="App">
       <header className="App-header">
+      {greeting}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          My first REACT App
+          My first REACT {learning} App
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href={url}
           target="_blank"
           rel="noopener noreferrer"
         >
