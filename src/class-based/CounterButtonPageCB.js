474df0c8eff826a082345bac7a6747fd321848be
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-24 13:08:21 +0530

    added state in class based components

--- src/class-based/CounterButtonPageCB.js
+++ src/class-based/CounterButtonPageCB.js
@@ -3,14 +3,23 @@ import { CounterButtonCB } from "./CounterButtonCB";
 import { CongratulationsMessageCB } from "./CongratulationsMessageCB";
 
 export class CounterButtonPageCB extends React.Component {
+    state = {
+        hideMessage: false,
+        numberOfClicks: 0
+    }
+
+    increment = () => {
+        this.setState({numberOfClicks: this.state.numberOfClicks + 1});
+    }
     render() {
+        const {hideMessage, numberOfClicks} = this.state;
         return (
             <>
             <h1> CounterButton Page!</h1>
              <DisplayIf condition={!hideMessage && numberOfClicks >= 10}>
               <CongratulationsMessage 
                threshold={10} 
-               onHide={() => setHideMessage(true)}/>
+               onHide={() => this.setState({hideMessage : true})}/>
               </DisplayIf> 
               <CounterButton onIncrement={increment} numberOfClicks={numberOfClicks}/> 
             </>
