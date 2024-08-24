40f1be92ac94cba72f5fc04b3a76ec1f07685bdd
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-24 20:36:11 +0530

    add UseEffect hook for localstorage

--- src/pages/CounterButtonPage.js
+++ src/pages/CounterButtonPage.js
@@ -1,18 +1,23 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { CounterButton } from "../CounterButton";
 import { CongratulationsMessage } from "../CongratulationsMessage";
 import { useParams, useLocation } from "react-router-dom";
-import queryString from 'query-string';
 import { DisplayIf } from "../DisplayIf";
 
 export const CounterButtonPage = () => {
-    const [numberOfClicks, setNumberOfClicks] = useState(0);
+    const [numberOfClicks, setNumberOfClicks] = useState(
+      Number(localStorage.getItem('numberOfClicks')) || 0
+      );
     const [hideMessage, setHideMessage] = useState(false);
     
+    useEffect(() => {
+      localStorage.setItem('numberOfClicks', numberOfClicks);
+    }, [numberOfClicks]);
+
     const increment = () => setNumberOfClicks(numberOfClicks+1);
   
-   
-    const location = useLocation();
+    
+    //const location = useLocation();
     //console.log(queryString.parse(location.search));
     return (
       <>
