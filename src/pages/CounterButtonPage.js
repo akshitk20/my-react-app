5256027769fdaf94fbae27da5b8dae920521c642
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-24 21:00:07 +0530

    added usePersistentState custom hook

--- src/pages/CounterButtonPage.js
+++ src/pages/CounterButtonPage.js
@@ -3,11 +3,13 @@ import { CounterButton } from "../CounterButton";
 import { CongratulationsMessage } from "../CongratulationsMessage";
 import { useParams, useLocation } from "react-router-dom";
 import { DisplayIf } from "../DisplayIf";
+import { usePersistentState } from "../usePersistentState";
 
 export const CounterButtonPage = () => {
     const [numberOfClicks, setNumberOfClicks] = useState(
       Number(localStorage.getItem('numberOfClicks')) || 0
       );
+    //const [numberOfClicks, setNumberOfClicks] = usePersistentState(numberOfClicks,0,Number);
     const [hideMessage, setHideMessage] = useState(false);
     
     useEffect(() => {
---
+++ src/usePersistentState.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export const usePersistentState = (storageKey, defaultValue, Type) => {
+    const [state, setState] = useState(
+        Type(localStorage.getItem(storageKey)) || defaultValue
+        );
+      
+    useEffect(() => {
+        localStorage.setItem(storageKey, state);
+    }, [state]);
+
+    return [state, setState];
+      
+}
